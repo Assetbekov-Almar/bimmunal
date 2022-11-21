3c6736fd94cd96b3d5be68647df6cbadf79f3829
Assetbekov-Almar <almar.asetbekov@gmail.com>
2022-11-21 16:12:38 +0600

    Feature: Add mongoose and handle register route

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,9 @@
-const express = require('express')
-
 require('dotenv').config()
+
+const express = require('express')
+const connectDB = require('./config/db')
+
+connectDB()
 
 const app = express()
 
@@ -10,6 +13,12 @@ app.use('/api/auth', require('./controllers/auth/auth.router'))
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
 	console.log('Listening on port ' + PORT)
 })
+
+process.on('unhandledRejection', (err) => {
+	console.log("unhandledRejection error: " + err)
+	server.close(() => process.exit(1))
+})
+
