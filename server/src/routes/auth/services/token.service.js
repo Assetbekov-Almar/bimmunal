bcd5d34381a25e04e6ee4b0c5bee8bdcdb734553
Assetbekov-Almar <almar.asetbekov@gmail.com>
2022-11-22 16:53:18 +0600

    Feature: Add service for tokens and mail

---
+++ server/src/routes/auth/services/token.service.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+const Token = require('../../../models/Token')
+
+class TokenService {
+	generateTokens(payload) {
+		const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: process.env.JWT_ACCESS_EXPIRE})
+		const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: process.env.JWT_ACCESS_EXPIRE})
+
+		return {
+			accessToken,
+			refreshToken
+		}
+	}
+
+	async saveToken(userId, refreshToken) {
+		const tokenData = await Token.findOne({ user: userId })
+
+		if (tokenData) {
+			tokenData.refreshToken = refreshToken
+			return tokenData.save()
+		}
+
+		const token = await Token.create({ user: userId, refreshToken })
+		return token
+	}
+}
+
+module.exports = new TokenService()
