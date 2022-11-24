fae83c93ebdd0f1cd2f02d505e8fae48184a8a9d
Assetbekov-Almar <almar.asetbekov@gmail.com>
2022-11-24 12:10:20 +0600

    Feature: Add basic login form

--- client/app/auth/page.tsx
+++ client/app/auth/page.tsx
@@ -1,50 +1,67 @@
 'use client'
 
-import { Formik } from 'formik'
+import { ErrorMessage, Formik } from 'formik'
+import styles from './Auth.module.css'
+import * as Yup from 'yup'
+import { Form, Field } from 'formik'
 
-const Auth = () => (
-	<div>
-		<h1>Anywhere in your app!</h1>
-		<Formik
-			initialValues={{ email: '', password: '' }}
-			validate={(values) => {
-				const errors = {}
-				if (!values.email) {
-					errors.email = 'Required'
-				} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-					errors.email = 'Invalid email address'
-				}
-				return errors
-			}}
-			onSubmit={(values, { setSubmitting }) => {
-				setTimeout(() => {
-					alert(JSON.stringify(values, null, 2))
-					setSubmitting(false)
-				}, 400)
-			}}
-		>
-			{({
-				values,
-				errors,
-				touched,
-				handleChange,
-				handleBlur,
-				handleSubmit,
-				isSubmitting,
-				/* and other goodies */
-			}) => (
-				<form onSubmit={handleSubmit}>
-					<input type='email' name='email' onChange={handleChange} onBlur={handleBlur} value={values.email} />
-					{errors.email && touched.email && errors.email}
-					<input type='password' name='password' onChange={handleChange} onBlur={handleBlur} value={values.password} />
-					{errors.password && touched.password && errors.password}
-					<button type='submit' disabled={isSubmitting}>
-						Submit
-					</button>
-				</form>
-			)}
-		</Formik>
-	</div>
-)
+type State = {
+	email: string
+	password: string
+}
+
+const initialValues: State = {
+	email: '',
+	password: '',
+}
+
+const validationSchema = Yup.object({
+	email: Yup.string().email('invalid email').required('required'),
+	password: Yup.string().required('required'),
+})
+
+const Auth = () => {
+	const onSubmit = (values: State, submittingObject: any) => {
+		console.log(values)
+	}
+
+	return (
+		<div className={styles.container}>
+			<Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+				{(formik) => {
+					return (
+						<Form className={styles.form}>
+							<div>
+								<label htmlFor='email'>Email : </label>
+								<Field
+									type='email'
+									name='email'
+									className={
+										formik.errors.email && formik.touched.email ? `${styles.input} ${styles.error}` : styles.input
+									}
+								/>
+								<ErrorMessage name='email' component='div' className={styles.input_feedback} />
+							</div>
+							<div style={{ marginTop: '10px' }}>
+								<label htmlFor='password'>Пароль : </label>
+								<Field
+									type='password'
+									name='password'
+									className={
+										formik.errors.password && formik.touched.password ? `${styles.input} ${styles.error}` : styles.input
+									}
+								/>
+								<ErrorMessage name='password' component='div' className={styles.input_feedback} />
+							</div>
+							<button type='submit' disabled={!formik.isValid || formik.isSubmitting}>
+								Войти
+							</button>
+						</Form>
+					)
+				}}
+			</Formik>
+		</div>
+	)
+}
 
 export default Auth
